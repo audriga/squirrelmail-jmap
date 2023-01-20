8f19fe84ffca38db7c66f1b9aebc45167fa8756e
Joris Baum <joris@audriga.com>
2023-01-20 10:43:41 +0100

    Also print_r($due) so we can identify the cause

--- src/Adapter/SquirrelMailTasksAdapter.php
+++ src/Adapter/SquirrelMailTasksAdapter.php
@@ -46,7 +46,7 @@ class SquirrelMailTasksAdapter extends AbstractAdapter {
 
         // If the second parsing attempt failed as well, log and return null
         if ($dueDate === false) {
-            $this->logger->error("There was an issue with parsing the due date from SQMail");
+            $this->logger->error("Unable to parse due date: " . print_r($due));
             return null;
         }
 
